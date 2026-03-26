03dff31218ecfa7eb81a5df97c041d79f0216086
abdou <abdennour.ghelbi@univ-constantine2.dz>
2026-03-26 01:03:12 +0100

    Remove backend files from root to reorganize

--- users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('signup/', views.signup, name='signup'),
-    path('login/', views.login_api, name='login'),
-    path('admin/pending-users/', views.get_pending_users, name='get_pending_users'),
-    path('admin/manage/<int:user_id>/', views.admin_manage_user, name='admin_manage_user'),
-]
